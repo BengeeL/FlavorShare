762f3ad480900dd261e6714c9b24206b0c89a9e7
Benjamin Lefebvre <benjamin.lefebvre.1024@gmail.com>
2024-02-26 14:11:42 -0500

    Initial Commit

---
+++ FlavorShare/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FlavorShare
+//
+//  Created by Benjamin Lefebvre on 2024-02-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FlavorShare/FlavorShareApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlavorShareApp.swift
+//  FlavorShare
+//
+//  Created by Benjamin Lefebvre on 2024-02-26.
+//
+
+import SwiftUI
+
+@main
+struct FlavorShareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
